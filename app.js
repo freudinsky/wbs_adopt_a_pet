0dfe4f86e4dec3795295de7ed42eca8512088c04
freudinsky <timo.fuchs94@icloud.com>
2023-09-22 11:31:07 +0200

    ..

---
+++ app.js
@@ -0,0 +1,34 @@
+const pets = require('./petList.js')
+const express = require('express')
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send(`<h1>Adopt a pet!</h1> 
+    <p>Browse through the links below to find your new furry friend.</p> 
+    <ul>
+    <li><a href="/animals/dogs">Dogs</a></li>
+    <li><a href="/animals/cats">Cats</a></li>
+    <li><a href="/animals/rabbits">Rabbits</a></li>
+</ul>`);
+})
+
+app.get("/animals/:pet_type", (req,res) => {
+    const petType = req.params.pet_type
+    const petTypeArr = pets[petType]
+    const petMapped = petTypeArr.map((pet) => `<li><a href="/animals/${petType}/${pet.name}">${pet.name}</a></li>`).join('')
+    res.send(`<h1>List of ${petType}</h1>
+    <ul>
+    ${petMapped}
+    </ul>`)
+})
+
+app.get("/animals/:pet_type/:pet_id", (req, res) => {
+    const petType = req.params.pet_type
+    const petName = req.params.pet_id
+    const pet = pets[petType].find((e)=> e.name == petName)
+    res.send(
+			`<h1>${pet.name}</h1><img src="${pet.url}"/><p>${pet.description}</p><ul><li>Breed: ${pet.breed}</li><li>Age: ${pet.age} years</li></ul>`
+		);
+})
+
+const server = app.listen(8000, ()=> console.log("Server running :8000"))
